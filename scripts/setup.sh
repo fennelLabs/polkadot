4acaf5578507c5625c1ee8232c8dd5f4b6315c75
Sean Batzel <romulus108@protonmail.com>
2023-06-06 23:11:39 -0400

    Dependency updates. This version compiles on Apple Silicon.

---
+++ scripts/setup.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -e
+
+git submodule init
+git submodule update
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | bash -s -- -y
+
+echo "*** Initializing WASM build environment"
+
+if [ -z $CI_PROJECT_NAME ] ; then
+   rustup update nightly
+   rustup update stable
+fi
+
+rustup override set nightly-2022-11-15
+rustup target add wasm32-unknown-unknown --toolchain nightly-2022-11-15

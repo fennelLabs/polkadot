7a5f0038650a270f4fc61f5532e95551745b9706
Sean Batzel <romulus108@protonmail.com>
2023-06-22 23:36:01 -0400

    Removed test scripts.

--- boot.sh
+++
@@ -1,4 +0,0 @@
- #!/bin/bash
-/app/target/release/polkadot key insert --base-path /tmp/relay/alice --chain /app/chainspec.json --key-type babe --scheme sr25519 --suri "coral intact chalk flash neither aunt prepare slender label tuna deposit rapid"
-/app/target/release/polkadot key insert --base-path /tmp/relay/alice --chain /app/chainspec.json --key-type gran --scheme ed25519 --suri "coral intact chalk flash neither aunt prepare slender label tuna deposit rapid"
-/app/target/release/polkadot --validator --base-path /tmp/relay/alice --chain /app/chainspec.json --port 30333 --rpc-port 9944 --telemetry-url "wss://telemetry.polkadot.io/submit/ 0" --name FnlDot0 --node-key 5b9c74d6cade40cc1a1a60ed06693945c3cde94775715a01e85fe2d4289a0453 --rpc-cors all --prometheus-external --rpc-methods Unsafe --rpc-external
--- validator-2.sh
+++
@@ -1,4 +0,0 @@
- #!/bin/bash
-/app/target/release/polkadot key insert --base-path /tmp/relay/charlie --chain /app/chainspec.json --key-type babe --scheme sr25519 --suri "hip top kick mule title lawn add magic donkey galaxy invite blouse"
-/app/target/release/polkadot key insert --base-path /tmp/relay/charlie --chain /app/chainspec.json --key-type gran --scheme ed25519 --suri "hip top kick mule title lawn add magic donkey galaxy invite blouse"
-/app/target/release/polkadot --validator --base-path /tmp/relay/charlie --chain /app/chainspec.json --port 30335 --rpc-port 9946 --bootnodes /ip4/127.0.0.1/tcp/30333/p2p/12D3KooWFhL6eiQU24RM5p35FcZNSAE3UccKiE88yweD4Z2PD746 --telemetry-url "wss://telemetry.polkadot.io/submit/ 0" --rpc-methods Unsafe --name FnlDot2 --rpc-external --prometheus-external --rpc-cors all

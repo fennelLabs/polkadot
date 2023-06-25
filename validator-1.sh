45b2291318cafee4016085d3b83dc43436952e0a
Sean Batzel <romulus108@protonmail.com>
2023-06-25 16:22:25 -0400

    Dependency update and rebuild.

--- validator-1.sh
+++ validator-1.sh
@@ -1,4 +1,4 @@
  #!/bin/bash
 ./target/release/polkadot key insert --base-path /tmp/relay/bob --chain ./chainspec.json --key-type babe --scheme sr25519 --suri "$(gsutil cat gs://whiteflag-0-admin/fennel-validator-key.sh)"
 ./target/release/polkadot key insert --base-path /tmp/relay/bob --chain ./chainspec.json --key-type gran --scheme ed25519 --suri "$(gsutil cat gs://whiteflag-0-admin/fennel-validator-key.sh)"
-./target/release/polkadot --validator --base-path /tmp/relay/bob --chain ./chainspec.json --port 30334  --rpc-port 9945 --bootnodes /ip4/127.0.0.1/tcp/30333/p2p/12D3KooWLj8FgbwDtLQbA5ozWqqx6vM1n2rmjwQMczSiDveyWa5G --rpc-methods Unsafe --name FnlDot1 --rpc-external --prometheus-external --rpc-cors all -ldebug
+./target/release/polkadot --validator --base-path /tmp/relay/bob --chain ./chainspec.json --port 30334  --rpc-port 9945 --bootnodes /ip4/127.0.0.1/tcp/30333/p2p/12D3KooWLj8FgbwDtLQbA5ozWqqx6vM1n2rmjwQMczSiDveyWa5G --rpc-methods Unsafe --name FnlDot1 --rpc-external --prometheus-external --rpc-cors all -ltrace
--- validator-2.sh
+++ validator-2.sh
@@ -1,4 +1,4 @@
  #!/bin/bash
 ./target/release/polkadot key insert --base-path /tmp/relay/charlie --chain ./chainspec.json --key-type babe --scheme sr25519 --suri "$(gsutil cat gs://whiteflag-0-admin/fennel-validator-2-key.sh)"
 ./target/release/polkadot key insert --base-path /tmp/relay/charlie --chain ./chainspec.json --key-type gran --scheme ed25519 --suri "$(gsutil cat gs://whiteflag-0-admin/fennel-validator-2-key.sh)"
-./target/release/polkadot --validator --base-path /tmp/relay/charlie --chain ./chainspec.json --port 30335 --rpc-port 9946 --bootnodes /ip4/127.0.0.1/tcp/30333/p2p/12D3KooWLj8FgbwDtLQbA5ozWqqx6vM1n2rmjwQMczSiDveyWa5G --rpc-methods Unsafe --name FnlDot2 --rpc-external --prometheus-external --rpc-cors all -ldebug
+./target/release/polkadot --validator --base-path /tmp/relay/charlie --chain ./chainspec.json --port 30335 --rpc-port 9946 --bootnodes /ip4/127.0.0.1/tcp/30333/p2p/12D3KooWLj8FgbwDtLQbA5ozWqqx6vM1n2rmjwQMczSiDveyWa5G --rpc-methods Unsafe --name FnlDot2 --rpc-external --prometheus-external --rpc-cors all -ltrace
